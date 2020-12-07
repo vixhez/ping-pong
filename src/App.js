5022430a9afdab9b8a3cdf9fff2e3073f445919b
Victoria Herod <vicky_herod@live.com>
2020-12-07 14:45:31 +0000

    Pingpong challenges - set up, initial state and increment working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = ({ 
+            player1,
+            player2,
+            player1Scores,
+            player2Scores
+          }) => (
+  <>
+      {/* header */}
+      <header className="jumbotron mt-4 mb-0">
+          <h1>PongPing</h1>
       </header>
-    </div>
-  );
-}
+
+      {/* scores */}
+      <div className="row mb-4">
+          <div className="col-md-6 mt-4">
+              <div className="card text-center bg-dark text-white">
+                  <h5 className="card-header">Player 1</h5>
+                  <div className="card-body">
+                      <p className="card-text display-1">{ player1 }</p>
+                  </div>
+                  <div className="card-footer">
+                      <button className="form-control btn btn-success"
+                              onClick={ player1Scores }>
+                                +
+                      </button>
+                  </div>
+              </div>
+          </div>
+
+          <div className="col-md-6 mt-4">
+              <div className="card text-center">
+                  <h5 className="card-header">Player 2</h5>
+                  <div className="card-body">
+                      <p className="card-text display-1">{ player2 }</p>
+                  </div>
+                  <div className="card-footer">
+                      <button className="form-control btn btn-success"
+                              onClick={ player2Scores }>
+                                +
+                      </button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      { /* winner message */}
+      <h2 className="alert alert-success">Player {/* winning player here */} wins!</h2>
+
+      <hr />
+
+      { /* reset button */}
+      <button className="btn btn-danger">Reset</button>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useReducer } from 'react';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  player1: 0,
+  player2: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PLAYER_1_SCORED": return {
+      ...state,
+      player1: state.player1 + 1
+    };
+    case "PLAYER_2_SCORED": return {
+      ...state,
+      player2: state.player2 + 1
+    };
+    default: return state;
+  }
+}
+
+const store = createStore(reducer, initialState);
+
+const render = () => {
+  const state = store.getState();
+  
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        player1={ state.player1 }
+        player2={ state.player2 }
+        player1Scores={ () => store.dispatch({ type: "PLAYER_1_SCORED" }) }
+        player2Scores={ () => store.dispatch({ type: "PLAYER_2_SCORED" })} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
